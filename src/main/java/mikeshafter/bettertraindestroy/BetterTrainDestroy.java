3c52df98f131db02ccb47457e39cf6108fbebabd
Mineshafter61 <yuhang.061@gmail.com>
2021-06-20 22:54:31 +0900

    NOW IT WORKS

--- src/main/java/mikeshafter/bettertraindestroy/BetterTrainDestroy.java
+++ src/main/java/mikeshafter/bettertraindestroy/BetterTrainDestroy.java
@@ -1,56 +1,96 @@
 package mikeshafter.bettertraindestroy;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public final class BetterTrainDestroy extends JavaPlugin {
+public final class BetterTrainDestroy extends JavaPlugin implements CommandExecutor {
   
   boolean destroy;
+  final long fullWarningDelay = getConfig().getLong("destroy")-getConfig().getLong("warning")/20;
+  long warningDelay;
+  BossBar bossBar;
   
   @Override
   public void onEnable() {
     // Plugin startup logic
+    getServer().getConsoleSender().sendMessage("§aTrainDestroy§b by Mineshafter61: v1.0");
+    getConfig().options().copyDefaults(true);
+    saveConfig();
     
     // TODO: destroy
     destroy = true;
+    warningDelay = (getConfig().getLong("destroy") - getConfig().getLong("warning")) / 20;
     
-    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-      public void run() {
-        Bukkit.broadcastMessage(ChatColor.GREEN+"§b[§aTrainDestroy§b] §fTrains will be destroyed in 3 minutes!");
-        Bukkit.broadcastMessage(ChatColor.GREEN+"§b[§aTrainDestroy§b] §fIf you are currently riding a train, please get off at the next stop.");
-        for (Player player : Bukkit.getOnlinePlayers()) {
-          player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.5f);
-        }
+    // 3 min warning
+    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+      Bukkit.broadcastMessage(String.format("§b[§aTrainDestroy§b] §fTrains will be destroyed in %s seconds!", warningDelay ));
+      Bukkit.broadcastMessage("§b[§aTrainDestroy§b] §fIf you are currently riding a train, please get off at the next stop.");
+      bossBar = Bukkit.createBossBar(String.format("§fTrains will be destroyed in %s seconds!", warningDelay), BarColor.BLUE, BarStyle.SOLID, BarFlag.DARKEN_SKY);
+      for (Player player : Bukkit.getOnlinePlayers()) {
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.5f);
+        bossBar.addPlayer(player);
       }
-    }, 72000, 216000);
-    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
-      public void run(){
-        for (Player player : Bukkit.getOnlinePlayers()) {
-          player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.2f);
-        }
+    }, getConfig().getLong("warning"), getConfig().getLong("period"));
+    
+    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+      for (Player player : Bukkit.getOnlinePlayers()) {
+        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f, 1.2f);
       }
-    }, 72004, 216000);
-    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
-      public void run(){
-        if (destroy){
-          ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-          Bukkit.broadcastMessage(ChatColor.GREEN+"§b[§aTrainDestroy§b] §fTrains have been destroyed!");
-          Bukkit.dispatchCommand(console, "train destroyall");
-          Bukkit.dispatchCommand(console, "ekillall minecarts world");
-        } else {
-          destroy = true;
-          Bukkit.broadcastMessage(ChatColor.GREEN+"§b[§aICIWI§b] §fTrainDestroy rescheduled, trains will be destroyed in the next cycle.");
-        }}}, 73200, 216000);
+    }, getConfig().getLong("warning")+4, getConfig().getLong("period"));
+    
+    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+      if (warningDelay > 0) {
+        warningDelay--;
+        bossBar.setTitle(String.format("§fTrains will be destroyed in %s seconds!", warningDelay));
+        bossBar.setProgress((double) warningDelay/(double) fullWarningDelay);
+      }
+    }, 0, 20);
+    
+    // Destroyer
+    Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+      if (destroy){
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+        Bukkit.broadcastMessage("§b[§aTrainDestroy§b] §fTrains have been destroyed!");
+        Bukkit.dispatchCommand(console, "train destroyall");
+        Bukkit.dispatchCommand(console, "ekillall minecarts world");
+        warningDelay = (getConfig().getLong("destroy") - getConfig().getLong("warning")) / 20;
+      } else {
+        destroy = true;
+        Bukkit.broadcastMessage("§b[§aTrainDestroy§b] §fTrainDestroy rescheduled, trains will be destroyed in the next cycle.");
+      }
+    }, getConfig().getLong("destroy"), getConfig().getLong("period"));
+  }
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (command.getName().equalsIgnoreCase("TrainDestroyDelay") && sender.hasPermission("TrainDestroy.delay")) {
+      destroy = false;
+      Bukkit.broadcastMessage("§b[§aTrainDestroy§b] §fTrainDestroy has been rescheduled!");
+      return true;
+    }
+    
+    else if (command.getName().equalsIgnoreCase("reload") && sender.hasPermission("TrainDestroy.delay")) {
+      reloadConfig();
+      saveConfig();
+    }
+    return false;
   }
   
   @Override
   public void onDisable() {
     // Plugin shutdown logic
+    getServer().getConsoleSender().sendMessage("§cTrainDestroy§4 by Mineshafter61: Thank you for using!");
   }
   
 
